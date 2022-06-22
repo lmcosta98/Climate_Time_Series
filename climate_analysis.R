6ebc7bc96be5b463415208d60071708aa83fefa0
Luís Miguel Costa <47531536+lmcosta98@users.noreply.github.com>
2022-06-22 22:30:09 +0100

    Update climate_analysis.R

--- climate_analysis.R
+++ climate_analysis.R
@@ -22,9 +22,44 @@ var(train$wind_speed)
 
 plot.ts(train, plot.type = 'multiple') # the time series plot confirms our hipothesis
 
-temp_series <- train[c(0:2)]
+meantemp_series <- ts(train$meantemp, start = c(2013, 1) , frequency = 365)
+humidity_series <- ts(train$humidity, start = c(2013, 1) , frequency = 365)
+wind_series <- ts(train$wind_speed, start = c(2013, 1) , frequency = 365)
+#meantemp_series
 
-temp_series <- as.ts(temp_series, start=c(temp_series$date[0]), end=c(temp_series$date[-1]))
+# Decomposing the series in its components
+#decomposition = stl(meantemp_series, s.window = 'per')
+# meantemp
+plot(stl(meantemp_series, s.window = 'per')) # it clearly has seasonality and also a trend
+# humidity
+plot(stl(humidity_series, s.window = 'per')) # it clearly has seasonality and also a trend
+# wind speed
+plot(stl(wind_series, s.window = 'per')) # it clearly has seasonality and also a trend
 
-stl(temp_series, s.window = 'per')
+
+# Calculating the first differences
+fdiff_meantemp = diff(meantemp_series)
+var(fdiff_meantemp)
+mean(fdiff_meantemp)
+plot.ts(cbind(meantemp_series,fdiff_meantemp), main="")
+
+
+fdiff_humidity = diff(humidity_series)
+var(fdiff_humidity)
+mean(fdiff_humidity)
+plot.ts(cbind(humidity_series,fdiff_humidity), main="")
+
+
+fdiff_wind = diff(wind_series)
+var(fdiff_wind)
+mean(fdiff_wind)
+plot.ts(cbind(wind_series,fdiff_wind), main="")
+
+
+# calculating the acfs
+acf2(fdiff_meantemp)
+acf2(fdiff_humidity)
+acf2(fdiff_wind)
+
+
 
